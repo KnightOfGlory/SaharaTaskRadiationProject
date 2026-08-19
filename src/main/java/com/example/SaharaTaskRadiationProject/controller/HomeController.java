93539b0eacbad0ac2d10da4aa19e428a754edba0
Knight faresksa44@gmail.com <202011373@stu.uob.edu.bh>
2026-08-19 13:03:12 +0300

    printing current port on page load

--- src/main/java/com/example/SaharaTaskRadiationProject/controller/HomeController.java
+++ src/main/java/com/example/SaharaTaskRadiationProject/controller/HomeController.java
@@ -1,10 +1,14 @@
 package com.example.SaharaTaskRadiationProject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("${server.port}")
+    private String port;
 
     @RequestMapping("/")
     public String index(){
@@ -14,6 +18,7 @@ public class HomeController {
     }
 
     private String getViewName(){
+        System.out.println("Server port: " + port);
         return "index.html";
     }
 
